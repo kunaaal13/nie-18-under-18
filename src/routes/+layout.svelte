9308c7b2c52eae0f7570b08c984150c627e8e713
Kunaaal13 <kunaaal.rao@gmail.com>
2026-01-18 22:44:35 +0530

    add fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon-16x16.png';
+	import favicon32 from '$lib/assets/favicon-32x32.png';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head><link rel="icon" href={favicon} />
+<link rel="apple-touch-icon" sizes="180x180" href={favicon} />
+<link rel="icon" type="image/png" sizes="32x32" href={favicon32} />
+<link rel="icon" type="image/png" sizes="16x16" href={favicon} />
+</svelte:head>
+
+
 {@render children()}
